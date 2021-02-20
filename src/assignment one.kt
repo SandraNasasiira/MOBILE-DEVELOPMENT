bbd20d1ae0b614f34139db31a9afb921fbd15155
Sandra Nasasiira <sandranasasiiraresty@gmail.com>
2021-02-20 23:20:24 +0300

    my assignment

---
+++ src/assignment one.kt
@@ -0,0 +1,15 @@
+fun main() {
+    val name=("sandra nasasiira")
+    print(name)
+    var age=19
+    print(age)
+    val phoneNumber=781658263
+    print(phoneNumber)
+    var weight=60.512437
+    print(weight )
+    val citizen=("false")
+    print(citizen )
+    val student=arrayOf("sandra nasasiira",19,781658263,60.512436,"false")
+    print(student)
+
+}
